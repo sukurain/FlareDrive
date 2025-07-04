3bed9a57e230938caa9404025cca4108dbeb7e13
sukurain <169013188+sukurain@users.noreply.github.com>
2025-07-04 20:46:19 +0800

    更新 Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -54,41 +54,4 @@ export default {
   fill: #444;
 }
 
-.stats {
-  margin: 10px 0;
-  color: #444;
-}
-
-.project-links {
-  font-size: 14px;
-  color: #444;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 15px;
-  text-align: center;
-}
-
-.project-links a {
-  color: #444;
-  text-decoration: none;
-}
-
-.project-links a:hover {
-  color: #222;
-}
-
-.powered {
-  margin-top: 10px;
-  color: #444;
-}
-
-.powered a {
-  color: #444;
-  text-decoration: none;
-}
-
-.powered a:hover {
-  color: #222;
-}
 </style>
